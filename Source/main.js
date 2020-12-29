bad714b1bbbd71e6088d9d8db9229e82fcba7fb9
Cuong <hungcuongkai123@gmail.com>
2020-12-29 22:26:09 +0700

    success
    connect with analytics data

--- Source/main.js
+++ Source/main.js
@@ -1,12 +1,28 @@
 // Read enviroment file
+
 require('dotenv').config({path: '../.env'});
 
 const mongoose = require("mongoose");
+const client = mongoose.MongoClient;
 
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("we're connected!");
-});
+
+
+
+db.once("open",()=>{
+const use_schema = new mongoose.Schema({});
+const Modules = mongoose.model("customer",use_schema);
+//console.log()
+Modules.find().limit(10).exec((err,docs) => {console.log(docs)})
+
+})
+// Modules
+// db.then((resolve)=>{
+
+
+
+// console.log(db)
+// })
+//   .catch((err)=>{console.log(err)})
